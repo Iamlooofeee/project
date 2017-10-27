734898968e36f42bfaaf30ce14336c9e8bd039a9
Iamlooofeee <iamlooofeee@mail.ru>
2017-10-27 21:17:21 +0300

    v4

--- js/require_youtube.js
+++ js/require_youtube.js
@@ -1,10 +1,13 @@
 (function() {
 	var stringSearch = "";
+	var idVideo = "";
 	search_button.onclick = function(e){
 		e.preventDefault();
 		if (!search_text.value){
 			console.log("Строка пуста");
 		} else {
+
+			// Get iformation about videos request for watches
 			stringSearch = search_text.value;
 			var request = gapi.client.youtube.search.list({
 			type: 'video',
@@ -14,18 +17,37 @@
 			order: 'viewCount'
 			});
 
-			var text;
-			var textWrapper = document.createElement("div");
-
 			request.execute(function(response) {
 				var resultSearch = response.result;
 				for (var key in resultSearch.items) {
+					idVideo += resultSearch.items[key].id.videoId;
 					console.log(resultSearch.items[key]);
 				}
-			})
-		};
+			});
+
+			// Get information about watches of videos
+
+			var requestWatches = gapi.client.youtube.videos.list({
+			type: 'video',
+			part: 'contentDetails,statistics',
+			order: 'viewCount',
+			maxResults: 3,
+			id: idVideo,
+			});
+			idVideo = "";
+
+			requestWatches.execute(function(response) {
+				var resultSearch = response.result;
+				for (var key in resultSearch.items) {
+					console.log(1)
+					console.log(resultSearch.items[key].statistics.likeCount);
+				}
+			});
+		};	
 	};
+
 })();
+
 function init(){
 	gapi.client.load("youtube", "v3",function(){
 		
