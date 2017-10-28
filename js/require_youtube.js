99cbead6eeb34653499852e9ef67320de85ac59f
Iamlooofeee <iamlooofeee@mail.ru>
2017-10-28 22:13:48 +0300

    v5

--- js/require_youtube.js
+++ js/require_youtube.js
@@ -7,42 +7,10 @@
 			console.log("Строка пуста");
 		} else {
 
-			// Get iformation about videos request for watches
 			stringSearch = search_text.value;
-			var request = gapi.client.youtube.search.list({
-			type: 'video',
-			part: 'snippet',
-			q:encodeURIComponent(stringSearch).replace(/%20/g, "+"),
-			maxResults:3,
-			order: 'viewCount'
-			});
+			var watchesVideos;
+			getInformation(stringSearch);
 
-			request.execute(function(response) {
-				var resultSearch = response.result;
-				for (var key in resultSearch.items) {
-					idVideo += resultSearch.items[key].id.videoId;
-					console.log(resultSearch.items[key]);
-				}
-			});
-
-			// Get information about watches of videos
-
-			var requestWatches = gapi.client.youtube.videos.list({
-			type: 'video',
-			part: 'contentDetails,statistics',
-			order: 'viewCount',
-			maxResults: 3,
-			id: idVideo,
-			});
-			idVideo = "";
-
-			requestWatches.execute(function(response) {
-				var resultSearch = response.result;
-				for (var key in resultSearch.items) {
-					console.log(1)
-					console.log(resultSearch.items[key].statistics.likeCount);
-				}
-			});
 		};	
 	};
 
@@ -54,3 +22,44 @@ function init(){
 	});
 	gapi.client.setApiKey("AIzaSyAo6BNS_UCYrLS_sEq0O3-jusWqfmAyhWY");
 };
+
+function getInformation(search) {
+			var request = gapi.client.youtube.search.list({
+			type: 'video',
+			part: 'snippet',
+			q:encodeURIComponent(search).replace(/%20/g, "+"),
+			maxResults:3,
+			order: 'viewCount'
+			});
+
+			request.execute(function(response) {
+				var resultSearch = response.result;
+				for (var key in resultSearch.items) {
+
+					idVideo = resultSearch.items[key].id.videoId;
+					getCountView(idVideo);
+					console.log(resultSearch.items[key]);
+				}
+			});
+}
+
+function getCountView(id) {
+			var requestWatches = gapi.client.youtube.videos.list({
+			type: 'video',
+			part: 'contentDetails,statistics',
+			order: 'viewCount',
+			maxResults: 3,
+			id: id,
+			});
+			idVideo = "";
+
+			requestWatches.execute(function(response) {
+				var resultSearch = response.result;
+				for (var key in resultSearch.items) {
+
+					watchesVideos = resultSearch.items[key].statistics.viewCount;
+					console.log("//" + watchesVideos);
+
+				}
+			});
+}
